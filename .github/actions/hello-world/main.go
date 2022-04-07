8d9da59853e6deaa545045d787b4a354a3f74271
dans002 <60510988+dans002@users.noreply.github.com>
2022-04-07 21:58:30 +0900

    Update main.go

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
   fmt.Println("Hello Docker Actions")
+  
+  // Access Inputs as environment vars
+  firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
+  secondGreeting := os.Getenv("INPUT_SECONGREETING")
+  thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
+  
+  // Use those inputs in the action logic
+  fmt.Println("Hello " + firstGreeting)
+  fmt.Println("Hello " + secondGreeting)
+  
+  //someimes inputs are not "required" and we can build around that
+  if thirdGreeting != "" {
+    fmt.Println("Hello " + thirdGreeting)
+  }
 }
